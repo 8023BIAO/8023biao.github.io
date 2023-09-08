17bc21bc7b79acf53e45dcafbc0f7794cc2af828
Biao <49405327+8023BIAO@users.noreply.github.com>
2023-09-08 17:21:14 +0800

    Update file via AndroLua

--- GGSrcipt/tools/filterSciNotaion.lua
+++ GGSrcipt/tools/filterSciNotaion.lua
@@ -4,14 +4,15 @@ local r = gg.getResults(gg.getResultCount())
 if #r > 0 then
   local blockSize = 1024*10
   local numBlocks = math.ceil(#r / blockSize)
+  numBlocks=0 and 1
   for blockIndex = 1, numBlocks do
     local startIdx = (blockIndex - 1) * blockSize + 1
     local endIdx = math.min(blockIndex * blockSize, #r)
     for j = startIdx, endIdx do
       local valueTable=r[j]
       local value=tostring(valueTable.value)
-      if value:find("inf") or value:find("E") then
-        delTable[#delTable+1]=valueTable
+      if value:find("inf") or value:find("E") or value:find("e") then
+        delTable[#delTable+1]=valueTable      
       end
     end
   end
