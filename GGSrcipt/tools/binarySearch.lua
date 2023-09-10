34994f29e04a57055925d5b3cbf87f27b6692f2f
Biao <49405327+8023BIAO@users.noreply.github.com>
2023-09-10 22:03:37 +0800

    Update file via AndroLua

--- GGSrcipt/tools/binarySearch.lua
+++ GGSrcipt/tools/binarySearch.lua
@@ -1,38 +1,14 @@
 --GG二分搜索法 by:biao
-
 local gg = _G["gg"]
-
-local function showChoiceDialog(t,s)
-  local name,fun = {},{}
-  for i = 1, #t, 2 do
-    name[#name+1] = t[i]
-    fun[#fun+1] = t[i + 1]
-  end
-  local m = gg.choice(name,nil,s)
-  for k, v in pairs(fun) do
-    if m == k then
-      v()
-    end
-  end
-end
-
 local function binarySearch()
   local rlc = gg.getResultsCount()
-  if rlc <2 then
-    return
-  end
+  if rlc <2 then return end
   local promptValues = gg.prompt({"类型(B1,W2,D4,X8,F16,Q32,E64):","改为:","冻结"},{"","",false},{"number","number","checkbox"})
-  if promptValues then
-    gg.setVisible(false)
-    promptValues[1],promptValues[2] = tonumber(promptValues[1]),tonumber(promptValues[2])
-   else
-    return
-  end
-  local is = true
-  local low = 0
-  local high = rlc
-  local mid,backup,set,get,select
-  while low < high do
+  if not promptValues then return end
+  gg.setVisible(false)
+  promptValues[1],promptValues[2] = tonumber(promptValues[1]),tonumber(promptValues[2])
+  local is,low,high,mid,backup,set,get,select = true,0,rlc
+  while (low < high) do
     if is then
       is=false
       mid = math.floor((low + high) / 2)
@@ -40,33 +16,26 @@ local function binarySearch()
       backup,set = {},{}
       for c,v in ipairs(get) do
         backup[#backup+1] = v
-        set[#set+1] = {
-          flags = promptValues[1],
-          value = promptValues[2],
-          freeze = promptValues[3],
-          address = v.address
-        }
+        set[#set+1] = {flags = promptValues[1],value = promptValues[2],freeze = promptValues[3],address = v.address}
       end
       gg.setValues(set)
     end
     if gg.isVisible() then
+      gg.setVisible(false)
       local select_range = high - mid
       is=true
-      gg.setVisible(false)
-      showChoiceDialog({
-        "选择",function()
-          select=true
-          low = mid + 1
-        end,
-        "过滤",function()
-          high = mid
-        end,
-      },mid .. "~" .. high .. ":" .. select_range)
+      local choice=gg.choice({"选择","过滤"},nil,mid .. "~" .. high .. ":" .. select_range)
+      if choice == 1 then
+        select = true
+        low = mid + 1
+       elseif choice == 2 then
+        high = mid
+      end
       gg.setValues(backup)
-      if low == high and select_range == 1 and select then
-        local set2 = {gg.getResults(rlc)[high]}
-        set2[1].name="过滤地址"
-        gg.addListItems(set2)
+      if high == low and select_range == 1 and select then
+        local save = {gg.getResults(rlc)[high]}
+        save[1].name="only"
+        gg.addListItems(save)
         break
       end
      else
@@ -74,5 +43,4 @@ local function binarySearch()
     end
   end
 end
-
 binarySearch()
