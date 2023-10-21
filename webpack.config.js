ffa13cda1ef7e2ca18f8e020ec6fed3a1a8bb011
Biao <49405327+8023BIAO@users.noreply.github.com>
2023-10-21 11:46:12 +0800

    Delete file via AndroLua

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './src/lua-string-decode.js',
-    mode: 'production',
-    output: {
-        filename: 'lua-string-decode.bundle.js',
-        library: 'LuaStringDecode'
-    }
-};
