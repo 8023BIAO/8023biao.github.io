be467efdbafd6b156647306ac608f2291d3e21d5
Biao <49405327+8023BIAO@users.noreply.github.com>
2024-08-13 17:35:19 +0800

    Update file via AndroLua

--- lua/TVBox/main.lua
+++ lua/TVBox/main.lua
@@ -2,7 +2,7 @@
 import "android.webkit.WebSettings"
 
 --local url = "http://x4jdm.top/"
-local url = "https://www.jianfast.com/"
+local url = "https://www.howdz.xyz/"
 
 local wv=LuaWebView(activity)
 wv.setBackgroundColor(0xff000000);
